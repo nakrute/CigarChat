72642abbf1584ac30f4932ba07b2c0b67abe7daf
nakrute <nakrute@gmail.com>
2021-08-08 12:36:57 -0400

    Finishing video 15, need to know why the delete buttons don't show up or are showing up in white

--- app/src/main/java/com/nkrute/cigarchat/adapters/AdapterChat.java
+++ app/src/main/java/com/nkrute/cigarchat/adapters/AdapterChat.java
@@ -1,24 +1,37 @@
 package com.nkrute.cigarchat.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.nkrute.cigarchat.R;
 import com.nkrute.cigarchat.models.ModelChat;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -50,7 +63,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
+    public void onBindViewHolder(@NonNull MyHolder myHolder, final int i) {
         //get data
         String message = chatList.get(i).getMessage();
         String timeStamp = chatList.get(i).getTimestamp();
@@ -70,6 +83,31 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
 
         }
 
+        // click to show delete dialog
+        myHolder.messageLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Delete");
+                builder.setMessage("Delete this message?");
+                //delete button
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteMessage(i);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                // create and show dialog
+                builder.create().show();
+            }
+        });
+
         //set seen/delivered status of message
         if (i==chatList.size()-1) {
             if (chatList.get(i).isSeen()) {
@@ -82,6 +120,37 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
         else {
             myHolder.isSeenTv.setVisibility(View.GONE);
         }
+    }
+
+    private void deleteMessage(int position) {
+        String myUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        String msgTimeStamp = chatList.get(position).getTimestamp();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
+        Query query = dbRef.orderByChild("timestamp").equalTo(msgTimeStamp);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    if (ds.child("sender").getValue().equals(myUID)) {
+                        ds.getRef().removeValue();
+
+                        //HashMap<String, Object> hashMap = new HashMap<>();
+                        //hashMap.put("message", "This message was deleted...");
+                        //ds.getRef().updateChildren(hashMap);
+
+                        Toast.makeText(context, "message deleted...", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, "You can only delete your messages", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public int getItemCount() {
@@ -103,6 +172,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
 
         ImageView profileIv;
         TextView messageTv, timeTv, isSeenTv;
+        LinearLayout messageLayout; // for click listener to show deleter
 
         public MyHolder(@NonNull View itemView)  {
             super(itemView);
@@ -111,6 +181,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.MyHolder> {
             messageTv = itemView.findViewById(R.id.messageTv);
             timeTv = itemView.findViewById(R.id.timeTv);
             isSeenTv = itemView.findViewById(R.id.isSeenTv);
+            messageLayout = itemView.findViewById(R.id.messageLayout);
 
         }
     }
