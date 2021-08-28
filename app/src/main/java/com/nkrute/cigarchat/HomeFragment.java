9f55b782c4142477a327dc1101dc35f128b43e0b
nakrute <nakrute@gmail.com>
2021-08-28 15:01:53 -0400

    Finishing video 18, and added known bugs list

--- app/src/main/java/com/nkrute/cigarchat/HomeFragment.java
+++ app/src/main/java/com/nkrute/cigarchat/HomeFragment.java
@@ -3,9 +3,12 @@ package com.nkrute.cigarchat;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -15,15 +18,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.nkrute.cigarchat.adapters.AdapterPosts;
+import com.nkrute.cigarchat.models.ModelPost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     //firebase auth
     FirebaseAuth firebaseAuth;
 
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPosts adapterPosts;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -37,9 +54,83 @@ public class HomeFragment extends Fragment {
         // init
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //recycler view and its properties
+        recyclerView = view.findViewById(R.id.postsRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //show newest post first and vice versa
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        postList = new ArrayList<>();
+
+        loadPosts();
+
         return view;
     }
 
+    private void loadPosts() {
+        //path of all posts
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        //get all data from this ref
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    postList.add(modelPost);
+
+                    //adapter
+                    adapterPosts = new AdapterPosts(getActivity(), postList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //in case of error
+                Toast.makeText(getActivity(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchPosts(String searchQuery) {
+        //path of all posts
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        //get all data from this ref
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    if (modelPost.getpTitle().toLowerCase().contains(searchQuery.toLowerCase()) || modelPost.getpDescr().toLowerCase().contains(searchQuery.toLowerCase())) {
+                        postList.add(modelPost);
+                    }
+
+                    postList.add(modelPost);
+
+                    //adapter
+                    adapterPosts = new AdapterPosts(getActivity(), postList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //in case of error
+                Toast.makeText(getActivity(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     private void checkUserStatus() {
         // get current user
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -62,6 +153,41 @@ public class HomeFragment extends Fragment {
     }
 
 
+    public void onCreateCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)MenuItemCompat.getActionView(item);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                // called when user presses search button
+                if (!TextUtils.isEmpty(s)) {
+                    searchPosts(s);
+                }
+                else {
+                    loadPosts();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //called as and when user presses any letter
+                if (!TextUtils.isEmpty(s)) {
+                    searchPosts(s);
+                }
+                else {
+                    loadPosts();
+                }
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
     /*handle menu item click*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
